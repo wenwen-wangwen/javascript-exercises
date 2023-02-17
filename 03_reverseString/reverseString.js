5400a514101661548846a8167052b6e9e2b464b6
Wenwen Wang <wenwen.wangwen@gmail.com>
2023-02-16 20:36:37 -0800

    Finish exercise 3 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  /* Method 1: for loop with string methods* */
+  // let stringReversed = '';
+  // for (let i = 0; i < string.length; i++) {
+  //   stringReversed += string.slice(string.length - 1 - i, string.length - i);
+  // }
+  // return stringReversed;
 
+  let array = string.split('');
+  array.reverse();
+  return array.join('');
 };
 
 // Do not edit below this line
