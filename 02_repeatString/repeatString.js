90ea3a8c418ef59b1e2b7e02cc57979137f7aaf2
Wenwen Wang <wenwen.wangwen@gmail.com>
2023-02-16 20:02:11 -0800

    Finish exercise 2 repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeatTimes) {
+  if (repeatTimes < 0) return 'ERROR';
+  let result = '';
+  for (let i = 0; i < repeatTimes; i++) {
+    result += word;
+  }
+  return result;
 };
 
 // Do not edit below this line
