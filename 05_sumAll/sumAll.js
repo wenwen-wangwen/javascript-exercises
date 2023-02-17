252670b3cabfba5e22f8a4f96de8c8fee9d526dc
Wenwen Wang <wenwen.wangwen@gmail.com>
2023-02-16 21:25:13 -0800

    Finish exercise 5 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') return 'ERROR';
+  if (num1 < 0 || num2 < 0) return 'ERROR';
+  if (num1 > num2) {
+    let temp = num2;
+    num2 = num1;
+    num1 = temp;
+  }
+  let sum = 0;
+  let currentNum = num1;
+  while (currentNum <= num2) {
+    sum += currentNum;
+    currentNum++;
+  }
+  return sum;
 
 };
 
