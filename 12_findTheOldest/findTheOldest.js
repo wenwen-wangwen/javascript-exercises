0425da161d62a95f85b19a97b7359cb14e212f8d
Wenwen Wang <wenwen.wangwen@gmail.com>
2023-02-20 21:18:41 -0800

    Finish exercise 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  array.sort((a, b) => {
+    const currentYear = new Date().getFullYear();
+    const ageA = (a.yearOfDeath || currentYear) - a.yearOfBirth;
+    const ageB = (b.yearOfDeath || currentYear) - b.yearOfBirth;
+    return ageB > ageA ? 1 : -1;
+  })
+  return array[0];
 
 };
 
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 2018,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
+findTheOldest(people);
 // Do not edit below this line
 module.exports = findTheOldest;
