89dad1262b7040239bc11edeeba7797e6aaf9166
Wenwen Wang <wenwen.wangwen@gmail.com>
2023-02-20 17:34:00 -0800

    Finish exercise 8 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current, 1);
 
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  return Math.pow(num, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+  let result = 1;
+  for (let i = num; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
