baaba1cb40f60677304cbd4f880db68b997d0d66
Wenwen Wang <wenwen.wangwen@gmail.com>
2023-02-20 20:43:26 -0800

    Complete exercise 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(val) {
+  const index = Number(val);
+  if (index < 0) return 'OOPS';
+  let a = 0;
+  let b = 1;
+  let sum;
+  for (let i = -1; i < index - 1; i++) {
+    sum = a + b;
+    b = a;
+    a = sum;
+  }
+  return sum;
 
 };
 
