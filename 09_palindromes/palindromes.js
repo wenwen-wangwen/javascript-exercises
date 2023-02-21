f043d0622087880b5853d2d8507c774f9f540cec
Wenwen Wang <wenwen.wangwen@gmail.com>
2023-02-20 18:05:51 -0800

    Finish exercise 9 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const sanitizedStr = str.toLowerCase().replaceAll(/[^a-z]/g, '');
+  const reversed = sanitizedStr.split('').reverse().join('');
+  return sanitizedStr === reversed;
 
 };
+palindromes("A car, a man, a maraca.");
 
 // Do not edit below this line
 module.exports = palindromes;
